91ddbf190140410e87adcd21fc1488d6fe9d0050
Saul Wiggin <saulwiggin@googlemail.com>
2015-07-24 12:23:09 +0100

    changes

--- uploadcsv.php
+++ uploadcsv.php
@@ -3,87 +3,56 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
-<php>
+<div class="page-header">
+	<h1> Upload CSV file </h1>
+</div>
+<?php
 
 // A code to upload a csv file with payroll information to a database and display the 
 // data to the user.
 
-// define database
-$servername = "localhost";
-$username = "saulwiggin";
-$password = "";
-$dbname = "upload";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
-
-// sql to create table
-$sql = "CREATE TABLE PayrollRecords (
-Staff_Member_Name VARCHAR(60) NOT NULL, 
-First_Name VARCHAR(60) NOT NULL,
-Middle_Name VARCHAR(60),
-Family_Name VARCHAR(60),
-Payroll_Ref VARCHAR (60),
-Total_Hours_Worked INT (6),
-Total_Pay DECIMAL(6,6),
-//THIS IS IN EPOCHS for unix timestamp
-Work_Date int(6)
-)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
 $file = fopen("upload.csv","r");
 
+$i = 0;
 
 while(! feof($file))
 
-  {
+  { 
 	
 	$array = fgetcsv($file);
 	
-	$three_names = explode($array[1]);
+	if ($i != 0){
+		
+		$three_names = explode(" ", $array[0]);
+		
+		if (count($three_names) != 3) {
+			//echo "No Middle Name";
+			array_push($three_names, "");
+		}
 
-	$sql = "INSERT INTO PayrollRecords (Staff_Member_Name, First_Name, Middle_Name, Family_Name, Payroll_Ref, Total_Hours_Worked, Total_Pay, Work_Date)
-	VALUES ($array[1], $three_names[1], $three_names[2], $three_names[3], $array[2], $array[3], $array[4], UNIX_TIMESTAMP($array[5]))";
-  
-	echo $array[1] . "</br>";
-	echo $three_names[1] . "</br>";
-	echo $three_names[2] . "</br>";
-	echo $three_names[3] . "</br>";
-	echo $array[2] . "</br>";
-	echo $array[3] . "</br>";
-	echo $array[4] . "</br>";
-	echo $array[5] . "</br>";
+	/* 	print_r($array);
+		print_r($three_names); */
+	
+		$sql = "INSERT INTO PayrollRecords (Staff_Member_Name, First_Name, Middle_Name, Family_Name, Payroll_Ref, Total_Hours_Worked, Total_Pay, Work_Date)
+		VALUES ($array[0], $three_names[0], $three_names[1], $three_names[2], $array[1], $array[2], $array[3], UNIX_TIMESTAMP($array[4]))";
+	  
+		echo "<b>Staff Member Name:</b> " . $array[0] . "<br>";
+		//echo $three_names[0] . "<br>";
+		//echo $three_names[1] . "<br>";
+		//echo $three_names[2] . "<br>";
+		echo "Payroll Ref: " . $array[1] . "<br>";
+		echo "Total Hours Worked: " . $array[2] . "<br>";
+		echo "Total Pay: " . $array[3] . "<br>";
+		//format date
+		echo "Work Date: " . date('d m Y',strtotime($array[4])) . "<br><hr>";
+	}
+	
+	$i = $i + 1;
   
   }
 
-//test connection if record was created
-if ($conn->query($sql) === TRUE) {
-    echo "New record created";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
-
 fclose($file);
 
-<?php>
+?>
 </body>
 </html>
